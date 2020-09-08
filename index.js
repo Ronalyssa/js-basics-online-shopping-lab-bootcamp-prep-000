b3d3c36faecbce06655a585c83818eb130698068
glorious-number-5611 <rloiseau99@yahoo.com>
2020-09-08 16:41:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,20 +11,57 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var item1 = {
+  itemName: item,
+  itemPrice:Math.floor(Math.random() * 100)
+ };
+ cart.push(item1);
+return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length == 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }
+  else if (cart.length == 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  }
+  else if (cart.length > 2) {
+    var firstItem = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`
+    var lastItem = `, and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
+    var middleItems = ""
+    for (let i = 1; i < cart.length - 1; i++) {
+      middleItems = middleItems.concat(`, ${cart[i].itemName} at $${cart[i].itemPrice}`)
+    }
+    return firstItem + middleItems + lastItem
+  }
+  else {
+    return "Your shopping cart is empty."
+  }
 }
-
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
+// function total() {
+//   // write your code here
+//   cart3 = {};
+//   for() {
+//     return
+//   }
+// }
+//
+// function removeFromCart(item) {
+//   // write your code here
+//   if(condition) {
+//
+//   } else {
+//     return "The item is not in your cart."
+//   }
+// }
+//
+// function placeOrder(cardNumber) {
+//   // write your code here
+//   if(condition) {
+//     return `Your total cost is ${total}, which will be charged to the card ${cardNumber}`
+//   } else {
+//     return "Sorry, we don't have a credit card on file for you."
+//   }
+// }
